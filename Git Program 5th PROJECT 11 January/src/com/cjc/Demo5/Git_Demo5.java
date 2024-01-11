b2428a2c33e0fe56719c852856d617bac25fcd5a
rutur <rutur@RUTURAJ>
2024-01-11 16:19:40 +0530

    By Ruturaj

---
+++ Git Program 5th PROJECT 11 January/src/com/cjc/Demo5/Git_Demo5.java
@@ -0,0 +1,17 @@
+package com.cjc.Demo5;
+
+public class Git_Demo5 
+{
+	public void m5() 
+	{
+	System.out.println("method m5");	
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Main method");
+		Git_Demo5 g5 = new Git_Demo5();
+		g5.m5();
+	}
+
+}
